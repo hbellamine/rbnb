3e79ab2293e3058e1ddd6241e2ed3a018f37763c
hamza bellamine <hamza.bellamine@gmail.com>
2020-02-24 14:34:33 +0000

    controllers set

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/show'
+  get 'bookings/update'
+  get 'bookings/destroy'
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
+  get 'users/update'
+  get 'users/destroy'
+  get 'puppies/index'
+  get 'puppies/new'
+  get 'puppies/create'
+  get 'puppies/show'
+  get 'puppies/update'
+  get 'puppies/destroy'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
