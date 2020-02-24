4f472408617d898e2d3e396d93005b46ac0c0dd9
Fanny Argoud <argoud.fanny@gmail.com>
2020-02-24 16:34:41 +0000

    Search bar

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,5 +1,11 @@
 class PuppiesController < ApplicationController
   def index
+    if params[:search].present?
+      sql_search = "name ILIKE :search OR breed ILIKE :search"
+      @puppies = Puppy.where(sql_search, search: "%#{params[:search]}%")
+    else
+      @puppies = Puppy.all
+    end
   end
 
   def new
--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -1,3 +1,16 @@
 class Puppy < ApplicationRecord
-  belongs_to :user
+  belongs_to :user;
+
+# def self.search(search)
+#   if search
+#     breed_type = Puppy.find_by(breed: search)
+#     if breed_type
+#       self.where(breed: breed_type)
+#     else
+#       @puppies = Puppy.all
+#     end
+#   else
+#     @puppies = Puppy.all
+#   end
+# end
 end
